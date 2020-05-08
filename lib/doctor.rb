6a067938762a90f63db41b00235924e15820e6c3
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-08 11:58:13 -0400

    Defines classes about music and doctors

---
+++ lib/doctor.rb
@@ -0,0 +1,25 @@
+class Doctor
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select { |appt| appt.doctor == self }
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def patients
+    appointments.collect(&:patient)
+  end
+end
